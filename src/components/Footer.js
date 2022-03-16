eed0161eb9db7df95935ba0b4060ea83ef396de6
Achara Obinna Vincent <33299753+Slimcent@users.noreply.github.com>
2022-03-17 00:49:15 +0100

    added font awesome

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,7 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+
 export default function Footer() {
     return (
         <footer>
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,10 +1,28 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMessage } from '@fortawesome/free-solid-svg-icons'
+import {  } from '@fortawesome/free-solid-svg-icons'
+
+
+
 export default function Information() {
     return (
        <div className="info-container">
            <h3>Achara Obinna V</h3>
            <h6>Fullstack Developer</h6>
            <p>obinna.achara.com</p>
+           <div className='contact'>
+                <a className="">
+                    <FontAwesomeIcon icon={faMessage} />
+                    <p>Email</p>
+                </a>
+                <a>
+                    <div>
+                        <p>LinkedIn</p>
+                    </div>
+                </a>
+           </div>
            <div className="email">
+           
                Email
            </div>
        </div>
